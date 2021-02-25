d97e1d37d400753eb0b6bca28d9e5afebb87efc4
Kyle Hicks <m212760@usna.edu>
2021-02-25 11:09:06 -0500

    Initial Commit

---
+++ webserver.py
@@ -0,0 +1,44 @@
+# Webserver!
+
+from aiohttp import web
+import aiohttp_jinja2
+import jinja2
+from random import randint
+
+@aiohttp_jinja2.template('Title_page.html.jinja2')
+async def title(request):
+    return {}
+
+@aiohttp_jinja2.template('hobbies.html.jinja2')
+async def hobbies(request):
+    return {
+        "hobbies": ["waterskiing", "traveling", "hiking"]
+        }
+
+@aiohttp_jinja2.template('2.html.jinja2')
+async def two(request):
+    return {
+        'show': 'westworld'
+    }
+
+@aiohttp_jinja2.template('3.html.jinja2')
+async def three(request):
+    return {}
+
+
+def main():
+    app = web.Application()
+
+    aiohttp_jinja2.setup(app,
+                         loader=jinja2.FileSystemLoader('templates'))
+
+    app.add_routes([web.get('/home.html', title),
+                    web.get('/hobbies.html.jinja2', hobbies),
+                    web.get('/2.html.jinja2', two),
+                    web.get('/3.html.jinja2', three),
+                    web.static('/static','static',show_index=True)])
+    print("Welcome to Webserver 1.0")
+    web.run_app(app, host="127.0.0.1", port=3000)
+
+
+main()
