a1408525c7ec35676a1805cdac3ca566dd837690
Kyle Hicks <m212760@usna.edu>
2021-03-10 11:25:30 -0500

    working on database
    march 10

--- webserver.py
+++ webserver.py
@@ -6,6 +6,7 @@ import sqlite3
 
 @aiohttp_jinja2.template('Title_page.html.jinja2')
 async def title(request):
+    print("actually running")
     return {}
 
 @aiohttp_jinja2.template('hobbies.html.jinja2')
@@ -30,11 +31,31 @@ async def tweets(request):
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM tweets ORDER BY likes DESC")
     results = cursor.fetchall()
+    print(results)
     conn.close()
     return {"tweets": results}
 
-async def add_tweet(request)
-    return web.Response(text="Added Tweet!")
+async def add_tweet(request):
+    data = await request.post()
+    content = data['content']
+    query = "INSERT INTO tweets (content,likes) VALUES (\"%s\",0)" % content
+    print("QUERY: %s" % query)
+    conn = sqlite3.connect("table.db")
+    cursor = conn.cursor()
+    cursor.execute(query)
+    conn.commit()
+    print("The user tweeted %s" % content)
+    raise web.HTTPFound('/')
+
+async def like(request):
+    conn = sqlite3.connect('table.db')
+    cursor = conn.cursor()
+    tweet_id = request.query[id]
+    cursor.execute("SELECT likes FROM tweets WHERE id=%s" % tweet_id)
+    like_count = cursor.fetchone()[0]
+    cursor.execute("UPDATE tweets SET likes=%d WHERE id=%s" % (like_count +1, tweet_id))
+    print(request.query['id'])
+    raise web.HTTPFound('/')
 
 
 def main():
@@ -50,10 +71,11 @@ def main():
                     web.get('/2.html.jinja2', two),
                     web.get('/3.html.jinja2', three),
                     web.get('/tweets.html.jinja2', tweets),
-                    web.post('/tweet', add_tweet)
+                    web.get('/like',like),
+                    web.post('/tweet', add_tweet),
                     web.static('/static','static',show_index=True)])
-    print("Welcome to Webserver 1.0")
+    print("Welcome to Webserver 2.1")
 
-    web.run_app(app, host="127.0.0.1", port=3000)
+    web.run_app(app, host="127.0.0.1", port=3001)
 
 main()
